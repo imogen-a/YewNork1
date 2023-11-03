06d3514d84dcbc8689e391d9d6886335f8e29f20
Imogen <120662169+imogen-a@users.noreply.github.com>
2023-11-03 11:06:56 +0000

    re-added menu code
    the menu screen now runs and cuts to the new york1 scene

---
+++ Assets/menu assets/menu scripts/menuScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartButton()
+   {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void QuitGame()
+   {
+      Application.Quit();
+      Debug.Log("Quit");
+   }
+}
