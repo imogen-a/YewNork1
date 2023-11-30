7f6d42adc4611a58c101524e9b1445cf7440dc48
Imogen <120662169+imogen-a@users.noreply.github.com>
2023-11-30 12:41:06 +0000

    fixed file

--- Assets/scripts/countDown.cs
+++ Assets/scripts/countDown.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class countDown : MonoBehaviour
 {
     [SerializeField] GameObject panel;
     [SerializeField] Image timeImage;
-    [SerializeField] TMP_Text timeText;
+    [SerializeField] Text timeText;
     [SerializeField] float duration, currentTime;
    
     // Start is called before the first frame update
@@ -16,7 +15,7 @@ public class countDown : MonoBehaviour
     {
         panel.SetActive(false);
         currentTime = duration;
-        timeText.TMP_Text = currentTime.ToString();
+        timeText.text = currentTime.ToString();
         //changes the countdown text to the current time, converting the numbers to a string
         StartCoroutine(TimeIEn());
     }
@@ -27,7 +26,7 @@ public class countDown : MonoBehaviour
         //while the time left is more than 0,
         {
             timeImage.fillAmount = Mathf.InverseLerp(0, duration, currentTime);
-            timeText.TMP_Text = currentTime.ToString();
+            timeText.text = currentTime.ToString();
             //change the time text to reflect the current time
             yield return new WaitForSeconds(1f);
             currentTime --;
@@ -38,7 +37,7 @@ public class countDown : MonoBehaviour
 
     void OpenPanel()
     {
-        timeText.TMP_Text = "";
+        timeText.text = "";
         panel.SetActive(true);
     }
 }
