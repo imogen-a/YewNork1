ebcd000b4bee257a03e41518bbefc94c404f4287
Imogen <120662169+imogen-a@users.noreply.github.com>
2023-12-15 12:14:31 +0000

    trying to get the loading screen to show for level 2

---
+++ Assets/scripts/LoadingScene2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene2 : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public GameObject VideoBackground;
+    public Scrollbar LoadingBarFill;
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        LoadingScreen.SetActive(true);
+        VideoBackground.SetActive(true);
+        
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            LoadingBarFill.size = progressValue;
+
+
+           yield return null; 
+        }
+        if (operation.isDone)
+        {
+            Time.timeScale = 1f;
+        }
+
+    }
+}
