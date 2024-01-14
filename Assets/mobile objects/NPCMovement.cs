1f84ff04a1bb453ea7494dcad1f01f4dff2f0adf
Lidia <147718842+lidia-tg@users.noreply.github.com>
2024-01-14 17:40:33 +0000

    Adjusted camera, added chasing rat and NPC sounds

--- Assets/mobile objects/NPCMovement.cs
+++ Assets/mobile objects/NPCMovement.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Diagnostics;
 using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.AI;
@@ -12,19 +11,40 @@ public class NPCMovement : MonoBehaviour
     private NavMeshAgent _agent;
     private GameObject Player;
     private float runDistanceRange = 3.0f;
+    public static Collider _Collider;
 
     public float movementRange;
+
+    public static bool yewNorkCoroutineStarted = false;
+    public YewNorkController yewNorkController;
 
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         Player = GameObject.Find("Player");
+        _Collider = gameObject.GetComponent<Collider>();
+        yewNorkController = GetComponent<YewNorkController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Collider[] coliders = Physics.OverlapBox(this.transform.position, _Collider.bounds.extents);
+        for (int i = 0; i < coliders.Length; i++)
+        {
+            if (coliders[i].gameObject == Player)
+            {
+                if (yewNorkCoroutineStarted == false)
+                {
+                    yewNorkController.StartYewNorking();
+                    StartCoroutine(YewNorkCoroutine());
+                    yewNorkCoroutineStarted = true;
+                }
+            }
+        }
+
         if (_agent.remainingDistance <= _agent.stoppingDistance)
         {
             Vector3 point;
@@ -60,5 +80,12 @@ public class NPCMovement : MonoBehaviour
                 _agent.SetDestination(newPos);
             }
         }
+    }
+
+    IEnumerator YewNorkCoroutine()
+    {
+        yield return new WaitForSeconds(0.0f);
+        yewNorkCoroutineStarted = false;
+        yewNorkController.StopYewNorking();
     }
 }
