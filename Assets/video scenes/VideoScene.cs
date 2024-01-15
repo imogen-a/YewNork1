d99e4f5d16e90fe78d184f7c38c5402927a4f958
Imogen <120662169+imogen-a@users.noreply.github.com>
2024-01-15 23:12:37 +0000

    pls pls work

--- Assets/video scenes/VideoScene.cs
+++ Assets/video scenes/VideoScene.cs
@@ -7,17 +7,23 @@ using UnityEngine.SceneManagement;
 public class VideoScene : MonoBehaviour
 {
     [SerializeField]
+
+    public GameObject Continue;
     VideoPlayer myVideoPlayer;
     // Start is called before the first frame update
     void Start()
     {
         myVideoPlayer.loopPointReached += DoSomethingWhenVideoDone;
+        //Continue.SetActive(false);
     }
 
     // Update is called once per frame
     void DoSomethingWhenVideoDone(VideoPlayer vp)
     {
+        Continue.SetActive(true);
         Debug.Log("Yeahhh the cutscene is done");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+
     }
 }
